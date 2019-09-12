a58be4b4fbca71c8eddd2b4c78895c7e5d9ae68a
ZhongZzZzZ <1044723955@qq.com>
2019-09-12 13:55:45 +0800

    活动界面修改

--- src/pages/Activity/Article.vue
+++ src/pages/Activity/Article.vue
@@ -2,15 +2,14 @@
     <div>
         <el-row>
             <div class="service" v-for="(item,index) in articleList" :key="index">
-                <el-col  :span="3"  v-show="index % 2 ==1 ? true: false" class="floatLeft">&nbsp;</el-col>
-                <el-col :span="3" v-show="index % 2 ==0?true:false" >&nbsp;</el-col>
+                <el-col  :xl="3" :lg="2" :md="3" :sm="1" :xs="3"  class="floatLeft">&nbsp;</el-col>
+                <!--<el-col :span="3" v-show="index % 2 ==0?true:false" >&nbsp;</el-col>-->
                 <!--右浮占位栅格-->
-                <el-col :span="1" v-show="index % 2 ==0?true:false" class="floatLeft">&nbsp;</el-col>
+                <!--<el-col :span="1" v-show="index % 2 ==0?true:false" class="floatLeft">&nbsp;</el-col>-->
                 <!--左浮占位栅格-->
-                <el-col :span="3"  v-show="index % 2 ==1?true:false" class="floatRight">&nbsp;</el-col>
-                <el-col :xl="8" :lg="9" :md="8" :sm="7" :xs="18"  :class="index % 2 ==1?'floatRight':' '">
+                <!--<el-col :xl="3" :lg="3" :md="3" :sm="0" :xs="0"  v-show="index % 2 ==1?true:false" class="floatRight">&nbsp;</el-col>-->
+                <el-col :xl="8" :lg="9" :md="8" :sm="7" :xs="18"  >
                     <div >
-                        
                         <router-link class="title" :to="{path:'/article',query:{ articleId : item.articleId,Id: 1 }}" target="_blank">{{item.title}}</router-link>
                         <p>{{item.activityStartTime}} --- {{item.activityEndTime}}</p>
                             <el-image :src="item.posterUrl" class="serviceImg">
@@ -21,8 +20,8 @@
                     </div>
                 </el-col>
                 <!--用空格制造间隔-->
-                <el-col :xs="4" :sm="3" :md="1" :lg="1" >&nbsp;</el-col>
-                <el-col :xs="18" :sm="10" :md="9" :lg="8" :xl="8">
+                <el-col :xs="4" :sm="2" :md="2" :lg="2" :xl="2">&nbsp;</el-col>
+                <el-col :xs="18" :sm="10" :md="9" :lg="9" :xl="8">
                     <div class="article">
                         <span class="passage" v-html="Aritcle(item)">
                         </span>
@@ -107,22 +106,32 @@ export default {
     .article{
         line-height: 0.5rem;
         margin-top: 1.4rem;
+        position: relative;
+        overflow: hidden;
+        height: 520px;
+        width: 550px;
         .passage{
             display: block;
             height: 400px;
             width: 550px;
-            overflow: hidden;
+            overflow-x: hidden;
+            overflow-y: scroll;
+            right: -20px;
+            position: relative;
         }
         a{
             display: inline-block;
             color:$orange;
             margin-top: 1rem;
+            right: -20px;
+            position: relative;
         }
     }
     .serviceImg{
         margin:.6rem 0px 1rem 0px;
         text-align: center;
-
+        width:100%;
+        height:100%;
     }
 }
 @media (max-width: 1300px) {
